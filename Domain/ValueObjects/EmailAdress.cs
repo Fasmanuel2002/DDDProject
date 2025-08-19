456567e9b4b1cc1bc2666ddf9f4d55753f201765
Fasmanuel2002 <112648218+Fasmanuel2002@users.noreply.github.com>
2025-08-19 20:00:43 +0200

    Finish Part 3

---
+++ Domain/ValueObjects/EmailAdress.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Domain.ValueObjects
+{
+    public partial record EmailAdress
+    {
+        private const string EmailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.com$";
+        public string Value { get; init; }
+        private EmailAdress(string value)
+        {
+            Value = value;
+        }
+
+        public static EmailAdress? Create(string value)
+        {
+            if(string.IsNullOrEmpty(value) || !EmailAdressRegex().IsMatch(value) || value.Length == 0)
+            {
+                return null;
+            }
+
+            return new EmailAdress(value);
+
+        }
+
+        [GeneratedRegex(EmailPattern)]
+        private static partial Regex EmailAdressRegex();
+
+        
+    }
+}
---
+++ Domain/ValueObjects/PersonIdentification.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Domain.ValueObjects
+{
+    public class PersonIdentification
+    {
+        public const int DefaultLength = 9;
+        private const string IdentificationPattern = @"^[0-9]{8}[A-Z]$";
+    
+        public string Value { get; init; }
+        private PersonIdentification(string value) => Value = value;
+    
+        public static PersonIdentification? Create(string value)
+        {
+            if (string.IsNullOrEmpty(value) || (!PersonIdentificationPersonRegex().IsMatch(value) || value.Length > 9)
+            {
+                return null;
+            }
+            return new PersonIdentification(value);
+        }
+
+        [GeneratedRegex(IdentificationPattern)]
+        private static partial Regex PersonIdentificationPersonRegex();
+
+    }
+}
