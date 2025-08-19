d636fa3389b47740023c30b7e5e2917b90c4bfd8
Fasmanuel2002 <112648218+Fasmanuel2002@users.noreply.github.com>
2025-08-19 21:03:35 +0200

    Finished Application, added ApplicationAssemblyReference, Depency Injection and more

---
+++ Application/ApplicationAssemblyReference.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    //ásicamente, crea una referencia a la asamblea (Assembly) del proyecto Application
+    //Esto es lo que nos permite que podamos injectar las dependencias como Services AddMediaR
+
+    // Esta clase sirve para exponer una referencia al Assembly de Application.
+    // Se usa, por ejemplo, al registrar dependencias (como MediatR, AutoMapper, FluentValidation, etc.),
+    // para que puedan escanear automáticamente este proyecto en busca de handlers, perfiles y validadores.
+    public class ApplicationAssemblyReference
+    {
+        internal static readonly Assembly assembly = typeof(ApplicationAssemblyReference).Assembly;
+    }
+}
---
+++ Application/DepencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    // Esta clase expone un método de extensión para registrar en el contenedor de dependencias
+    // todos los servicios relacionados con el proyecto Application (MediatR, FluentValidation, etc.)
+    // utilizando ApplicationAssemblyReference como marcador de assembly.
+    public static class DepencyInjection
+    {
+
+
+
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            //Asi hemos injectado MediataR 
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssemblyContaining<ApplicationAssemblyReference>();
+            });
+            //Con esto ya tenemos injectado esto en las referencias 
+            services.AddValidatorsFromAssemblyContaining<ApplicationAssemblyReference>();
+
+            return services;
+        }
+    }
+}
