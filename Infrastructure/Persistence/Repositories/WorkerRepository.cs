27dbe90172b4e3b65d28571d4a357da7b7847455
Fasmanuel2002 <manuelfasruiz@gmail.com>
2025-08-21 13:35:56 +0200

    Infrastructure worked, video 8

---
+++ Infrastructure/Persistence/Repositories/WorkerRepository.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Workers;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    public class WorkerRepository : IWorkerRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public WorkerRepository(ApplicationDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+        public async Task AddWorkerAsync(Worker worker) => _context.Workers.Add(worker);
+
+
+        public async Task DeleteWorkerAsync(Worker worker) => _context.Workers.Remove(worker);
+
+
+        public Task<bool> ExistsAsync(WorkerId id) => _context.Workers.AnyAsync(worker => worker.Id == id);
+
+        public Task<List<Worker>> GetAllWorkersAsync() => _context.Workers.ToListAsync();
+
+        public Task<Worker?> GetByIdAsync(WorkerId Id) => _context.Workers.FirstOrDefaultAsync(worker => worker.Id == Id);
+
+        public async Task UpdateWorkerAsync(Worker worker) => _context.Workers.Update(worker);
+    }
+}
